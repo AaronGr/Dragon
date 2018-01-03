4cb2f0d06cd479e4ba3245669088ec4acf01e1a9
My_Desktop\Agrav <agrav12825@gmail.com>
2018-01-03 11:07:23 -0600

    Add Camera that Follows Player

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -2,15 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class CameraFollow : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+    public GameObject player;
+
+	void LateUpdate () {
+        transform.position = player.transform.position;
 	}
 }
